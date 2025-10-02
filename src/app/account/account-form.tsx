9ca4ab4f7446f61fa94a46b454452c037aacf848
swos13 <szymon.wos1319@gmail.com>
2025-10-02 16:54:17 +0200

    feat: user sign up and log in

---
+++ src/app/account/account-form.tsx
@@ -0,0 +1,126 @@
+"use client";
+
+import { createClient } from "@/utils/supabase/client";
+import { User } from "@supabase/supabase-js";
+import { useCallback, useEffect, useState } from "react";
+
+export default function AccountForm({ user }: { user: User | null }) {
+  const supabase = createClient();
+  const [loading, setLoading] = useState(true);
+  const [fullName, setFullName] = useState<string | null>(null);
+  const [username, setUsername] = useState<string | null>(null);
+  const [website, setWebsite] = useState<string | null>(null);
+  const [avatar_url, setAvatarUrl] = useState<string | null>(null);
+
+  const getProfile = useCallback(async () => {
+    try {
+      setLoading(true);
+
+      const { data, error, status } = await supabase.from("profiles").select("full_name, username, website, avatar_url").eq("id", user?.id).single();
+
+      if (error && status !== 406) {
+        console.log(error);
+        throw error;
+      }
+
+      if (data) {
+        setFullName(data.full_name);
+        setUsername(data.username);
+        setWebsite(data.website);
+        setAvatarUrl(data.avatar_url);
+      }
+    } catch (error) {
+      alert("Error while loading user data!");
+    } finally {
+      setLoading(false);
+    }
+  }, [user, supabase]);
+
+  useEffect(() => {
+    getProfile();
+  }, [user, getProfile]);
+
+  async function updateProfile({
+    username,
+    fullName,
+    website,
+    avatar_url,
+  }: {
+    username: string | null;
+    fullName: string | null;
+    website: string | null;
+    avatar_url: string | null;
+  }){
+    try {
+        setLoading(true)
+
+        const { error } = await supabase.from('profiles').upsert({
+            id: user?.id as string,
+            full_name: fullName,
+            username,
+            website,
+            avatar_url,
+            updated_at: new Date().toISOString(),
+        })
+        if(error) throw error
+        alert('Profile update!')
+    } catch(error) {
+        alert('Error while updating user data!')
+    } finally {
+        setLoading(false);
+    }
+  };
+
+  return (
+        <div className="form-widget">
+            {/* ... */}
+            <div>
+                <label htmlFor="email">Email</label>
+                <input id="email" type="text" value={user?.email} disabled />
+            </div>
+            <div>
+                <label htmlFor="fullName">Full Name</label>
+                <input
+                    id="fullName"
+                    type="text"
+                    value={fullName || ''}
+                    onChange={(e) => setFullName(e.target.value)}
+                />
+            </div>
+            <div>
+                <label htmlFor="username">Username</label>
+                <input
+                    id="username"
+                    type="text"
+                    value={username || ''}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+            </div>
+            <div>
+                <label htmlFor="website">Website</label>
+                <input
+                    id="website"
+                    type="url"
+                    value={website || ''}
+                    onChange={(e) => setWebsite(e.target.value)}
+                />
+            </div>
+            <div>
+                <button
+                    className="button primary block"
+                    onClick={() => updateProfile({ fullName, username, website, avatar_url })}
+                    disabled={loading}
+                >
+                    {loading ? 'Loading ...' : 'Update'}
+                </button>
+            </div>
+            <div>
+                <form action="/auth/signout" method="post">
+                    <button className="button block" type="submit">
+                        Sign out
+                    </button>
+                </form>
+            </div>
+        </div>
+    )
+}
